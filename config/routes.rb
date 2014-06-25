872fb229ed88977330cc42ec330fadd8930c3828
geansru <d@geans.ru>
2014-06-25 21:53:04 +0600

    Done with index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
   resources :markus
+  get 'markus/carousel', as: 'c'
 
   root 'markus#index'
   # The priority is based upon order of creation: first created -> highest priority.
